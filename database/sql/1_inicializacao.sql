7808b54fdaaa90d5d838f66727507b93e3a769c1
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2026-01-25 16:46:04 -0300

    Ajuste script de inicializacao

--- database/sql/1_inicializacao.sql
+++ database/sql/1_inicializacao.sql
@@ -1,6 +1,3 @@
-/* Criando Base */
-
--- Criar o database, se não existir
 CREATE DATABASE IF NOT EXISTS fiapx;
 
 USE fiapx;
@@ -9,8 +6,8 @@ CREATE USER 'user_fiap'@'%' IDENTIFIED BY 'pass_fiap';
 GRANT SELECT, INSERT, UPDATE, DELETE ON fiapx.* TO 'user_fiap'@'%';
 FLUSH PRIVILEGES;
 
-CREATE TABLE users (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
+CREATE TABLE  users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
@@ -19,19 +16,19 @@ CREATE TABLE users (
 );
 
 CREATE TABLE video_processings (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
-    user_id VARCHAR(36) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     original_file_name VARCHAR(255) NOT NULL,
     status ENUM('Pending', 'Processing', 'Completed', 'Failed') NOT NULL DEFAULT 'Pending',
-    s3_input_path VARCHAR(1024) NOT NULL,
-    s3_output_path VARCHAR(1024) NULL,
+    s3_input_path VARCHAR(2048) NOT NULL, 
+    s3_output_path VARCHAR(2048) NULL,
     failure_reason TEXT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    completed_at DATETIME NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,  
+    completed_at TIMESTAMP NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE INDEX idx_user_id ON video_processings(user_id);
 CREATE INDEX idx_status ON video_processings(status);
-
--- INSERT INTO users (id, name, email, password_hash) VALUES ('1', 'Admin User', 'admin@example.com', '');
+CREATE INDEX idx_user_status ON video_processings(user_id, status);
+CREATE INDEX idx_created_at ON video_processings(created_at); 
