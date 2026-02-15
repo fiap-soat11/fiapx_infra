dcc4a42fdb983ab47221ce1c38c5094b4567c5e5
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2026-02-15 15:16:12 -0300

    Alteracao parametro db_endpoint

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,9 +1,9 @@
-output "db_endpoint" {
-  description = "Database endpoint"
-  value       = aws_db_instance.mysql.endpoint
-}
-
-output "database_endpoint" {
-  description = "Database endpoint"
-  value       = module.database.db_endpoint
-}
+#output "db_endpoint" {
+#  description = "Database endpoint"
+#  value       = aws_db_instance.mysql.endpoint
+#}
+#
+#output "database_endpoint" {
+#  description = "Database endpoint"
+#  value       = module.database.db_endpoint
+#}
