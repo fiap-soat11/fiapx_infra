0c6d03b0ee529107350ff612f2c9c376c7778e1d
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2026-03-08 20:09:31 -0300

    Ajustando nome do bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-fiap-soat-___AWS_ACCOUNT_ID___"
+    bucket = "s3-fiap-soat11-___AWS_ACCOUNT_ID___"
     key    = "fiap/terraform.tfstate"
     region = "us-east-1"
   }
